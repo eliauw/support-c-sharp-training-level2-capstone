e0b15c3548ac37d799d542c5216805dcc7efe196
Eric <eric.liauw@ni.com>
2020-11-05 09:37:11 -0600

    Add PhaseAmplitudeOffset Class

---
+++ Source/L2CapstoneProject/PhaseAmplitudeOffset.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2CapstoneProject
+{
+    class PhaseAmplitudeOffset
+    {
+        public double Phase { get; set; }
+        public double Amplitude { get; set; }
+
+        public PhaseAmplitudeOffset(double PhaseOffset, double AmplitudeOffset)
+        {
+            Phase = PhaseOffset;
+            Amplitude = AmplitudeOffset;
+        }
+    }
+}
